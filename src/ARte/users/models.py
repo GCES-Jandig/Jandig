f19c0e26065805858d0ef934b2628aab5d8b82fe
manuengsf <31634620+manuengsf@users.noreply.github.com>
2020-12-07 15:14:41 -0300

    reapiring errors

--- src/ARte/users/models.py
+++ src/ARte/users/models.py
@@ -19,7 +19,7 @@ class Profile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
- if  not created:
+ if  created:
    Profile.objects.create(user=instance)
 
 
